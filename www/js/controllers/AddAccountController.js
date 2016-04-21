da9e2959b162ee79f23e637e237309ae586db3f4
Nilesh Jinde <nilesh.jinde1@wipro.com>
2016-04-21 20:03:20 +0530

    show password code added

---
+++ www/js/controllers/AddAccountController.js
@@ -0,0 +1,25 @@
+mainApp.controller('AddAccountController',function($scope,$rootScope,$location){
+	//alert("AddAccountController");
+
+	//alert($location);
+	$scope.currentPath = $location.path();
+	$scope.navTitle = "Add Account";
+
+	$scope.OnSubmitAddAccountForm = function(){
+		alert("On Add Account btn Clicked-- ");
+		$rootScope.back();
+	}
+	
+	$scope.showPwd = function() {
+       // alert("checkbox checked  "+$scope.showpassword);
+		var isCheckBoxChecked = $scope.showpassword;
+		if(isCheckBoxChecked){
+			$('#pwd').attr('type', 'text');
+			$('#pwd').attr('readonly', true);
+		}else{
+			$('#pwd').attr('type', 'password');
+			$('#pwd').attr('readonly', false);
+		}
+      };
+
+});
